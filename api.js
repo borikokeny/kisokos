ca6ccc6a922734ff42c3fd7db04014ef69bf901f
Bori Kokeny <bori@minerali.hu>
2023-11-22 11:38:38 +0100

    api

--- api.js
+++ api.js
@@ -1,12 +1,27 @@
 // Fetching API
 
-const url = "https://api.noroff.dev/docs/json/api/v1/auction/listings";
+// const url = "https://api.noroff.dev/docs/json/api/v1/auction/listings";
+const url = "https://cat-fact.herokuapp.com/facts";
+
+const resultsContainer = document.querySelector(".results");
 
 async function getBids() {
+
   const response = await fetch(url);
-  // const results = await response.json();
-  // console.log(results);
-  console.log(response);
+
+  const results = await response.json();
+
+  console.log(results);
+ 
+  // const facts = results.all;
+ 
+  resultsContainer.innerHTML = "";
+
+  for (let i = 0; i < results.length; i++) {
+    console.log(results[i].text);
+
+    resultsContainer.innerHTML += `<div class="result">${results[i].text}</div>`;
+  }
 }
 
 getBids();
