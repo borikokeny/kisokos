6a02f6a2c9b56ccd8bb866740ca0fce022df78f8
Bori Kokeny <bori@minerali.hu>
2024-01-08 13:12:50 +0100

    api

--- api.js
+++ api.js
@@ -5,23 +5,28 @@ const url = "https://cat-fact.herokuapp.com/facts";
 
 const resultsContainer = document.querySelector(".results");
 
-async function getBids() {
+// async function getBids() {
 
-  const response = await fetch(url);
+//   const response = await fetch(url);
 
-  const results = await response.json();
+//   const results = await response.json();
 
-  console.log(results);
+//   console.log(results);
  
-  // const facts = results.all;
+//   // const facts = results.all;
  
-  resultsContainer.innerHTML = "";
+//   resultsContainer.innerHTML = "";
 
-  for (let i = 0; i < results.length; i++) {
-    console.log(results[i].text);
+//   for (let i = 0; i < results.length; i++) {
+//     console.log(results[i].text);
 
-    resultsContainer.innerHTML += `<div class="result">${results[i].text}</div>`;
-  }
-}
+//     resultsContainer.innerHTML += `<div class="result">${results[i].text}</div>`;
+//   }
+// }
 
-getBids();
+// getBids();
+
+fetch(url)
+  .then(function(response) {
+    console.log(response);
+  })
